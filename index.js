e3f5b7367a2bf4e50d27e2128b3aca4e1bb7b195
Zakia Aktar <zakiaaktar@Zakias-MacBook-Pro.local>
2023-11-06 12:08:59 +0100

    service collection and services get api done

--- index.js
+++ index.js
@@ -14,27 +14,35 @@ app.use(express.json());
 //console.log(process.env.DB_PASS)
 
 
-const uri = "mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xwlx9fx.mongodb.net/?retryWrites=true&w=majority";
+//const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xwlx9fx.mongodb.net/?retryWrites=true&w=majority`;
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xwlx9fx.mongodb.net/squareBnb?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+
+        const serviceCollection = client.db('squareBnb').collection('services');
 
 
 
-    app.get('/api/v1/services', (req, res) => {
-        res.send('it is working')
-    });
+        app.get('/api/v1/services', async (req, res) => {
+            const cursor = serviceCollection.find()
+            const result = await cursor.toArray()
+            res.send(result)
+        });
 
 
 
@@ -46,21 +54,21 @@ async function run() {
 
 
 
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    //await client.close();
-  }
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        //await client.close();
+    }
 }
 run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
     res.send('square bnb server is running');
-  });
-  
-  app.listen(port, () => {
+});
+
+app.listen(port, () => {
     console.log(`square bnb server running on ${port}`);
-  })
+})
